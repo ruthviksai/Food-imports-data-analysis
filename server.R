2d1c0c32972de1b73fda41006fcb4709eee2a1fa
Ruthvik sai Mondreti <ruthvik@D-10-18-206-181.dhcp4.washington.edu>
2018-12-07 18:09:39 -0800

    Final Commit

--- server.R
+++ server.R
@@ -7,14 +7,15 @@ library(janitor)
 library(DT)
 
 setwd("~/Desktop/STAT302/")
-shinyServer <- function(input, output) {
 import <- read_excel("imports.xlsx")
 import_smaller <- import %>% select("Year", "Total animal foods", "Total plant foods", 
                                     "Total beverages", "Total U.S. food imports") %>% 
-                  mutate_if(is.numeric, ~round(., 1)) %>% clean_names()
+  mutate_if(is.numeric, ~round(., 1)) %>% clean_names()
+
+import_smaller[,-1] <- round(import_smaller[,-1],0)
+shinyServer <- function(input, output) {
   
 import_smaller[,-1] <- round(import_smaller[,-1],0)
-head(import_smaller)
 #graph <- xyplot('Total animal foods' ~ 'Year', data = import_smaller)
 
   output$graph1 <-renderPlotly({ 
