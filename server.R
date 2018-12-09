494e20a8ec1b8455d2b61fb24680b690e90d94b1
ruthvs1324 <44628597+ruthvs1324@users.noreply.github.com>
2018-12-08 22:31:45 -0800

    Update server.R

--- server.R
+++ server.R
@@ -6,7 +6,6 @@ library(plotly)
 library(janitor)
 library(DT)
 
-setwd("~/Desktop/STAT302/")
 import <- read_excel("imports.xlsx")
 import_smaller <- import %>% select("Year", "Total animal foods", "Total plant foods", 
                                     "Total beverages", "Total U.S. food imports") %>% 
@@ -19,24 +18,31 @@ import_smaller[,-1] <- round(import_smaller[,-1],0)
 #graph <- xyplot('Total animal foods' ~ 'Year', data = import_smaller)
 
   output$graph1 <-renderPlotly({ 
-    plot_ly(data = import_smaller, x = ~year, y = ~total_animal_foods, mode = "markers", type = "scatter")
+    plot_ly(data = import_smaller, x = ~year, y = ~total_animal_foods, mode = "markers", type = "scatter") %>%
+      layout(xaxis = list(title = "Year"), yaxis = list(title = "Animal Foods imports (Million $)"))
     
     })
   output$graph2 <-renderPlotly({ 
-    plot_ly(data = import_smaller, x = ~year, y = ~total_plant_foods, mode = "markers", type = "scatter")
+    plot_ly(data = import_smaller, x = ~year, y = ~total_beverages, mode = "markers", type = "scatter") %>%
+      layout(xaxis = list(title = "Year"), yaxis = list(title = "Beverages imports (Million $)"))
     
   })
   output$graph3 <-renderPlotly({ 
-    plot_ly(data = import_smaller, x = ~year, y = ~total_beverages, mode = "markers", type = "scatter")
+    plot_ly(data = import_smaller, x = ~year, y = ~total_plant_foods, mode = "markers", type = "scatter") %>%
+      layout(xaxis = list(title = "Year"), yaxis = list(title = "Plant Foods imports (Million $)"))
     
   })
   output$graph4 <-renderPlotly({ 
-    plot_ly(data = import_smaller, x = ~year, y = ~total_u_s_food_imports, mode = "markers", type = "scatter")
+    plot_ly(data = import_smaller, x = ~year, y = ~total_u_s_food_imports, mode = "markers", type = "scatter") %>%
+      layout(xaxis = list(title = "Year"), yaxis = list(title = "U.S. Total Food imports (Million $)"))
     
   })
   output$mytable = DT::renderDataTable({
     import_smaller
   })
 }
+
+
+
 
 
